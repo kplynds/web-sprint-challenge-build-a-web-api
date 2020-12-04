24c5ed6ab7ebbad9e13633f7305762c9abdbb232
Kyle Lynds <kylelynds@Kyles-MBP.lan>
2020-12-03 22:26:49 -0800

    chunk

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,66 @@
-// Write your "actions" router here!
+const router = require("express").Router()
+const Actions = require("./actions-model")
+
+router.get("/", (req, res) => {
+  Actions.get()
+  .then(data => {
+    res.status(200).json(data)
+  })
+  .catch(err => {
+    res.status(500).json({ err: err.message})
+  })
+})
+
+router.get("/:id", (req, res) => {
+  Actions.get(req.params.id)
+  .then(data => {
+    if(data) {
+      res.status(200).json(data)
+    } else {
+      res.status(404).json("Not Found")
+    }
+  })
+  .catch(err => {
+    res.status(404).json(err.message)
+  })
+})
+
+router.post("/", (req, res) => {
+  if(req.body.project_id && req.body.notes && req.body.description) {
+    Actions.insert(req.body)
+    .then(data => {
+      res.status(200).json(data)
+    })
+    .catch(err => {
+      res.status(400).json(err.message)
+    })
+  } else {
+    res.status(400)
+  }
+})
+
+router.put("/:id", (req, res) => {
+  if(req.body.id && req.body.project_id && req.body.description && req.body.notes) {
+    Actions.update(req.params.id, req.body)
+    .then(data => {
+      res.status(200).json(data)
+    })
+    .catch(err => {
+      res.status(500).json(err.message)
+    })
+  } else {
+    res.status(400)
+  }
+})
+
+router.delete("/:id", (req, res) => {
+  Actions.remove(req.params.id)
+  .then(data => {
+    res.status(200).json(data)
+  })
+  .catch(err => {
+    res.status(500).json(err.message)
+  })
+})
+
+module.exports = router
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,69 @@
-// Write your "projects" router here!
+const router = require("express").Router()
+const Projects = require("./projects-model")
+
+router.get("/", (req, res) => {
+  Projects.get()
+  .then(data => {
+    res.status(200).json(data)
+  })
+  .catch(err => {
+    res.status(500).json(err.message)
+  }) 
+})
+
+router.get("/:id", (req, res) => {
+  Projects.get(req.params.id)
+  .then(data => {
+    if(data) {
+      res.status(200).json(data)
+    } else {
+      res.status(404)
+    }
+    
+  })
+  .catch(err => {
+    res.status(500).json(err.message)
+  })
+})
+
+router.post("/", (req, res) => {
+  Projects.insert(req.body)
+  .then(data => {
+    res.status(201).json(data)
+  })
+  .catch(err => {
+    res.status(500).json(err.message)
+  })
+})
+
+router.put("/:id", (req, res) => {
+  Projects.update(req.params.id, req.body)
+  .then(data => {
+    res.status(200).json(data)
+  })
+  .catch(err => {
+    res.status(400).json(err.message)
+  })
+})
+
+router.delete("/", (req, res) => {
+  Projects.remove(req.id)
+  .then(() => {
+    res.status(200)
+  })
+  .catch(err => {
+    res.status(500).json(err.message)
+  })
+})
+
+router.get("/:id/actions", (req, res) => {
+  Projects.getProjectActions(req.params.id)
+  .then(data => {
+    res.status(200).json(data)
+  })
+  .catch(err => {
+    res.status(500).json(err.message)
+  })
+})
+
+module.exports = router
